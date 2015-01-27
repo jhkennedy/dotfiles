0e63fabc0d3de208680825d0797ceca92c28e26f
Joseph H Kennedy <kennedyjh@ornl.gov>
2015-01-27 13:19:22 -0500

    Add installation scripts

---
+++ Documents/Computers/runInstallScript.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# check input arguments
+if (( $# != 1 )) ; then
+    echo "Woopsies! Usage: $0 INSTALL_SCRIPT"
+    exit 1
+fi
+
+./${1} | tee ${1}.log
+
+# see if anything didn't install
+echo " "
+echo "=========================================="
+echo " Install finished... lets see how it went!"
+echo "=========================================="
+echo " "
+
+cat ubi1404_HPz420.log | sed -n '/Ign/!p' \
+                       | sed -n '/Hit/!p' \
+                       | sed -n '/Get/!p' \
+                       | sed -n '/Reading/!p' \
+                       | sed -n '/Building/!p' \
+                       | sed -n '/already the newest/!p' \
+                       | sed -n '/Requirement already satisfied/!p' \
+                       | sed -n '/Press/!p'
